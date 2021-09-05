87beb4095a9138660fe301c66dd1d16b1bddf979
George <george@wizardofweb.com>
2021-09-05 21:07:49 +0800

    added addresses page

--- assets/application.js
+++ assets/application.js
@@ -10,3 +10,26 @@ url.searchParams.set('sort_by', e.currentTarget.value);
 window.location = url.href;
 });
 }
+
+if (document.getElementById('AddressCountryNew') != null ) {
+  document.getElementById('AddressCountryNew').addEventListener('change', function(e){
+    var provinces = this.options[this.selectedIndex].getAttribute('data-provinces');
+    var provinceSelector = document.getElementById('AddressProvinceNew');
+    var provinceArray = JSON.parse(provinces);
+
+    if(provinceArray.length < 1) {
+      provinceSelector.setAttribute('disabled', 'disabled');
+    } else {
+
+    provinceSelector.removeAttribute('disabled')
+    }
+
+    provinceSelector.innerHTML = "";
+    var options = '';
+    for (var i=0; i < provinceArray.length; i++) {
+      options += '<option value="' + provinceArray[i][0] + '">' + provinceArray[i][0] + '</option>';
+    }
+
+    provinceSelector.innerHTML = options;
+  })
+}
